05accf1a9d895a020719590fff9fe264ce84fbe9
TakumiSuzuki <takumi.s.1230@icloud.com>
2023-10-06 14:56:16 +0900

    画像の更新

--- post.py
+++ post.py
@@ -1,9 +1,11 @@
 import requests
 
-url = 'http://127.0.0.1:3000/users/1/updatePicture'
-data = {'user_id': 1, 'name': 'test'}
+user_id = input("ユーザIDを入力してん")
+url = "http://127.0.0.1:3000/users/" + user_id + "/updatePicture"
+file_path = '/Users/apple/Downloads/7FUCBcl__400x400.png'  # アップロードするファイルのパス
 
-response = requests.patch(url, data=data)
+files = {'picture': open(file_path, 'rb')}
+response = requests.patch(url, files=files)
 
 if response.status_code == 200:
     print('Update successful')
